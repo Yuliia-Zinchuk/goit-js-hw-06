2c315b6cc55745bb694e7ea8fa2a5e91459e837b
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-26 00:07:17 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+
+let listArray = ingredients.map((ingredient) => {
+  const liIngredient = document.createElement("li");
+  liIngredient.textContent = ingredient;
+  liIngredient.classList.add("item");
+  return liIngredient;
+});
+listIngredients.append(...listArray);
