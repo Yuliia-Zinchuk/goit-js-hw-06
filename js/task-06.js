726960b8760bfc52125f09e2665d32c961fff8bb
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-27 13:59:15 +0200

    update task6

--- js/task-06.js
+++ js/task-06.js
@@ -2,12 +2,9 @@ const textInput = document.querySelector("#validation-input");
 const valueLength = textInput.getAttribute("data-length");
 
 textInput.addEventListener("blur", () => {
-  let textInputValue = textInput.value;
-  let userValueLength = textInputValue.length;
-
-  if (userValueLength === Number(valueLength)) {
-    textInput.classList.add("valid");
-    textInput.classList.replace("invalid", "valid");
+  if (textInput.value.length === Number(valueLength)) {
+    textInput.classList.add("valid") ||
+      textInput.classList.replace("invalid", "valid");
   } else {
     textInput.classList.add("invalid");
   }
