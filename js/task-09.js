1930c3509f80405c7048c760d982961c5a336749
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-27 13:59:27 +0200

    add task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const colorRandom = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  span.textContent = colorRandom;
+  body.style.backgroundColor = colorRandom;
 }
