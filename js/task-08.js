25bafb2f041e9b4559a4096e832bcea83608c4a7
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-27 12:12:49 +0200

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+const userData = {};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled. Please fil them out");
+  }
+
+  userData.email = email.value;
+  userData.password = password.value;
+
+  console.log(userData);
+  event.currentTarget.reset();
+}
