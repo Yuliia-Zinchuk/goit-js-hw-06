74754e9b00e2ee13d2de23392184266955a177ba
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-28 12:26:20 +0200

    Update task6 with ternarn

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,8 @@
 const textInput = document.querySelector("#validation-input");
-const valueLength = textInput.getAttribute("data-length");
 
 textInput.addEventListener("blur", () => {
-  if (textInput.value.length === Number(valueLength)) {
-    textInput.classList.add("valid") ||
-      textInput.classList.replace("invalid", "valid");
-  } else {
-    textInput.classList.add("invalid");
-  }
+  textInput.value.length === Number(textInput.dataset.length)
+    ? textInput.classList.add("valid") ||
+      textInput.classList.replace("invalid", "valid")
+    : textInput.classList.add("invalid");
 });
