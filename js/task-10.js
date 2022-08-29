0e147c8b16aa42bd8659d6439b7c8f14ed97cfbf
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-29 16:10:40 +0200

    Add task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const div = document.querySelector("#boxes");
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+
+const divArray = [];
+
+buttonCreate.addEventListener("click", () => {
+  createBoxes(Number(input.value));
+});
+
+function createBoxes(amount) {
+  let sizeWidth = 20;
+  let sizeHeight = 20;
+  for (let i = 1; i <= amount; i += 1) {
+    sizeWidth += 10;
+    sizeHeight += 10;
+
+    const divEl = document.createElement("div");
+
+    divEl.style.width = `${sizeWidth}px`;
+    divEl.style.height = `${sizeHeight}px`;
+    divEl.style.backgroundColor = `${getRandomHexColor()}`;
+
+    divArray.push(divEl);
+  }
+  div.append(...divArray);
+}
+
+buttonDestroy.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function destroyBoxes() {
+  div.innerHTML = "";
+  input.innerHTML = "";
 }
