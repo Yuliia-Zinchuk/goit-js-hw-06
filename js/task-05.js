3aafb2032e47c75fcfcc1f96753ee730e98e4f67
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-29 11:03:33 +0200

    change with ternarn

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,7 @@ const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  if (event.currentTarget.value === "") {
-    output.textContent = "Anonymous";
-  } else {
-    output.textContent = event.currentTarget.value;
-  }
+  event.currentTarget.value === ""
+    ? (output.textContent = "Anonymous")
+    : (output.textContent = event.currentTarget.value);
 });
